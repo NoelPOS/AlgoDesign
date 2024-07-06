5fe270e1e3a7a46f84f6d99eebe6a5485ff4d97d
Noel Paing Oak Soe <u6530183@au.edu>
2024-07-06 16:51:45 +0700

    practice

--- week 3/practice.py
+++ week 3/practice.py
@@ -1,15 +1,37 @@
-coins = list(map(int, input().split()))
-change = int(input())
-answer = float('inf')
-def mincoin(v):
- global answer
- if v <= 0:
-  return 0
- else:
-  for coin in coins:
-   diff = v - coin
-   result = 1 + mincoin(diff)
-   answer = min(answer, result)
+# coins = list(map(int, input().split()))
+# change = int(input())
 
-mincoin(change)
-print(answer)
+# def mincoin(v):
+
+#  if v == 0:
+#   return 0
+#  if v < 0:
+#   return float('inf')
+#  else:
+#   answer = float('inf')
+#   for coin in coins:
+#    diff = v - coin
+#    result = 1 + mincoin(diff)
+#    answer = min(answer, result)
+#   return answer
+
+# print(mincoin(change))
+
+
+prices = list(map(int, input().split()))
+
+def maxRev(l):
+ if l == 0:
+  return 0
+ if l < 0:
+  return float('-inf')
+ else:
+  max_rev = float('-inf')
+  for p in range(len(prices)):
+   diff = l - ( p + 1)
+   result = maxRev(diff) + prices[p]
+   max_rev = max(result, max_rev)
+
+  return max_rev
+ 
+print(maxRev(len(prices)))
